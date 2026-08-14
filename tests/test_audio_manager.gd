34619e7f10812ad804612a5e4f37e5cb4c4e0517
Rudenko Sergey <progenitor123@gmail.com>
2026-08-14 22:48:21 +0700

    test: исправить типизацию музыкальных плейлистов

--- tests/test_audio_manager.gd
+++ tests/test_audio_manager.gd
@@ -11,6 +11,12 @@ func before_each() -> void:
 	add_child_autofree(manager)
 
 
+func _playlist(streams: Array) -> Array[AudioStream]:
+	var typed_streams: Array[AudioStream] = []
+	typed_streams.assign(streams)
+	return typed_streams
+
+
 func test_play_sfx_with_null_stream_adds_no_children() -> void:
 	manager.play_sfx(null)
 	assert_eq(manager.get_child_count(), 0)
@@ -44,14 +50,14 @@ func test_play_sfx_supports_multiple_overlapping_calls() -> void:
 
 
 func test_play_music_playlist_with_empty_array_does_nothing() -> void:
-	manager.play_music_playlist("menu", [])
+	manager.play_music_playlist("menu", _playlist([]))
 	assert_eq(manager.get_child_count(), 0)
 
 
 func test_play_music_playlist_starts_first_track_on_master_bus() -> void:
 	var track_a := AudioStreamMP3.new()
 	var track_b := AudioStreamMP3.new()
-	manager.play_music_playlist("menu", [track_a, track_b])
+	manager.play_music_playlist("menu", _playlist([track_a, track_b]))
 	assert_eq(manager.get_child_count(), 1)
 	var player: AudioStreamPlayer = manager.get_child(0)
 	assert_eq(player.stream, track_a)
@@ -61,8 +67,8 @@ func test_play_music_playlist_starts_first_track_on_master_bus() -> void:
 func test_play_music_playlist_repeat_call_same_context_is_noop() -> void:
 	var track_a := AudioStreamMP3.new()
 	var track_b := AudioStreamMP3.new()
-	manager.play_music_playlist("menu", [track_a, track_b])
-	manager.play_music_playlist("menu", [track_a, track_b])
+	manager.play_music_playlist("menu", _playlist([track_a, track_b]))
+	manager.play_music_playlist("menu", _playlist([track_a, track_b]))
 	assert_eq(
 		manager.get_child_count(),
 		1,
@@ -73,7 +79,7 @@ func test_play_music_playlist_repeat_call_same_context_is_noop() -> void:
 func test_music_finished_advances_to_next_track_and_loops() -> void:
 	var track_a := AudioStreamMP3.new()
 	var track_b := AudioStreamMP3.new()
-	manager.play_music_playlist("menu", [track_a, track_b])
+	manager.play_music_playlist("menu", _playlist([track_a, track_b]))
 	var player: AudioStreamPlayer = manager.get_child(0)
 	assert_eq(player.stream, track_a)
 	manager._on_music_finished()
@@ -84,7 +90,7 @@ func test_music_finished_advances_to_next_track_and_loops() -> void:
 
 func test_stop_music_resets_active_state() -> void:
 	var track_a := AudioStreamMP3.new()
-	manager.play_music_playlist("menu", [track_a])
+	manager.play_music_playlist("menu", _playlist([track_a]))
 	assert_true(manager._music_active)
 	manager.stop_music()
 	assert_false(manager._music_active)
@@ -93,17 +99,17 @@ func test_stop_music_resets_active_state() -> void:
 func test_play_music_playlist_different_context_switches_track() -> void:
 	var track_a := AudioStreamMP3.new()
 	var track_b := AudioStreamMP3.new()
-	manager.play_music_playlist("menu", [track_a])
-	manager.play_music_playlist("battle", [track_b])
+	manager.play_music_playlist("menu", _playlist([track_a]))
+	manager.play_music_playlist("battle", _playlist([track_b]))
 	assert_eq(manager._music_player.stream, track_b, "Смена контекста должна переключить трек")
 
 
 func test_context_switch_crossfades_instead_of_cutting_old_track() -> void:
 	var track_a := AudioStreamMP3.new()
 	var track_b := AudioStreamMP3.new()
-	manager.play_music_playlist("menu", [track_a])
+	manager.play_music_playlist("menu", _playlist([track_a]))
 	var menu_player: AudioStreamPlayer = manager.get_child(0)
-	manager.play_music_playlist("battle", [track_b])
+	manager.play_music_playlist("battle", _playlist([track_b]))
 	assert_eq(
 		manager.get_child_count(),
 		2,
@@ -120,11 +126,13 @@ func test_context_switch_crossfades_instead_of_cutting_old_track() -> void:
 
 
 func test_crossfade_finished_stops_and_frees_old_player() -> void:
-	manager.play_music_playlist("menu", [AudioStreamMP3.new()])
-	manager.play_music_playlist("battle", [AudioStreamMP3.new()])
+	manager.play_music_playlist("menu", _playlist([AudioStreamMP3.new()]))
+	manager.play_music_playlist("battle", _playlist([AudioStreamMP3.new()]))
 	assert_not_null(manager._fading_out_player)
 	manager._on_crossfade_finished()
-	assert_null(manager._fading_out_player, "После завершения кроссфейда старый плеер освобождается")
+	assert_null(
+		manager._fading_out_player, "После завершения кроссфейда старый плеер освобождается"
+	)
 
 
 func test_play_battle_music_uses_battle_context() -> void:
